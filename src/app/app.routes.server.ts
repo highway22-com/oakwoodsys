3c9d9459bba0c18eed46a2d2827df797fd6aa738
Noe Alvarado <luisnoejasso@gmail.com>
2025-12-30 08:39:12 -0600

    fix build v1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,24 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Routes with parameters should use Server rendering (SSR) instead of Prerender
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'resources/case-studies/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'industries/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'services/:slug',
+    renderMode: RenderMode.Server
+  },
+  // Static routes can use Prerender
   {
     path: '**',
     renderMode: RenderMode.Prerender
